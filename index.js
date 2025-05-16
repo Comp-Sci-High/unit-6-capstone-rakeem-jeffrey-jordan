8c1c5fbd21941e4bb30e689994561b8c62510d12
RakeemG <rakeem.grace25@compscihigh.org>
2025-05-16 14:25:44 +0000

    hje1j

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
 
+const Item = mongoose.model('Item', new mongoose.Schema({
+  name: String,
+  image: String,
+  category: String,
+  price: Number,
+  purchaseLink: String
+}), 'Items')
+
+app.get('/api/items', async (req, res) => {
+  const items = await Item.find({})
+  res.json(items)
+})
+
+app.post('/api/items', async (req, res) => {
+  const newItem = await Item.create(req.body)
+  res.json(newItem)
+})
+
+app.delete('/api/items/:id', async (req, res) => {
+  const deletedItem = await Item.findByIdAndDelete(req.params.id)
+  res.json(deletedItem)
+})
