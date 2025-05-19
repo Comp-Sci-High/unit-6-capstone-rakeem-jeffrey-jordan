2295f3d8cff3c3bddd93a588dd03ab06414c8596
RakeemG <rakeem.grace25@compscihigh.org>
2025-05-19 02:54:35 +0000

    Working

--- index.js
+++ index.js
@@ -8,19 +8,32 @@ const Item = mongoose.model('Item', new mongoose.Schema({
   category: String,
   price: Number,
   purchaseLink: String
-}), 'Items')
+}), 'Items');
 
 app.get('/api/items', async (req, res) => {
-  const items = await Item.find({})
-  res.json(items)
-})
+  const items = await Item.find({});
+  res.json(items);
+});
 
 app.post('/api/items', async (req, res) => {
-  const newItem = await Item.create(req.body)
-  res.json(newItem)
-})
+  const newItem = await Item.create(req.body);
+  res.json(newItem);
+});
+
+app.patch('/api/items/:id', async (req, res) => {
+  const updatedItem = await Item.findByIdAndUpdate(
+    req.params.id,
+    req.body,
+    { new: true }
+  );
+  res.json(updatedItem);
+});
 
 app.delete('/api/items/:id', async (req, res) => {
-  const deletedItem = await Item.findByIdAndDelete(req.params.id)
-  res.json(deletedItem)
-})
+  const deletedItem = await Item.findByIdAndDelete(req.params.id);
+  res.json(deletedItem);
+});
+
+app.listen(3000, () => {
+  console.log('Server running.');
+});
