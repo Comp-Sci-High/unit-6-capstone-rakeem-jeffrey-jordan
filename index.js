1b954217ca184ed51ded626c43f51813b17c6f13
RakeemG <rakeem.grace25@compscihigh.org>
2025-05-21 14:06:41 +0000

    Milestone 3

--- index.js
+++ index.js
@@ -7,12 +7,18 @@ const Item = mongoose.model('Item', new mongoose.Schema({
   image: String,
   category: String,
   price: Number,
-  purchaseLink: String
+  purchaseLink: String,
+  size: String
 }), 'Items');
+
+app.get('/', async (req, res) => {
+  const items = await Item.find({});
+  res.render('Clothing', { items });
+});
 
 app.get('/api/items', async (req, res) => {
   const items = await Item.find({});
-  res.json(items);
+   res.json(items);
 });
 
 app.post('/api/items', async (req, res) => {
@@ -34,6 +40,13 @@ app.delete('/api/items/:id', async (req, res) => {
   res.json(deletedItem);
 });
 
-app.listen(3000, () => {
-  console.log('Server running.');
-});
+async function startServer() {
+   
+    await mongoose.connect("mongodb+srv://SE12:CSH2025@cluster0.xfcbvkb.mongodb.net/codealong?retryWrites=true&w=majority&appName=Cluster0");
+
+    app.listen(3000, () => {
+        console.log(`Server running.`);
+    });
+}
+
+startServer();
